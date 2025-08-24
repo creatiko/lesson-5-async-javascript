3145f1c65d7afb0a6a4e5c39e552f0538f2bd12f
creatiko <creatiko@outlook.com>
2025-08-24 17:27:36 -0400

    Feat: uploaded the solution.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,15 @@
-const grabNewMessages = () => {
-    //only edit this function
-    const notificationWindow = document.querySelector('.notificationAlerts').firstElementChild
-    const data =  simulateNetworkRequest()
-    notificationWindow.textContent = `You have ${data} new messages.`
+const grabNewMessages = async () => {
+  const notificationWindow = document.querySelector('.notificationAlerts').firstElementChild
+  notificationWindow.textContent = 'Loading...'
+  try {
+    const data = await simulateNetworkRequest()
+    notificationWindow.textContent = `You have ${data} new notification${(data === 1) ? '' : 's'}.`
+    console.log(data)
+  } catch (error) {
+    notificationWindow.textContent = `There was an error fetching your notifications`
+    console.error(error)
+  }
 }
-
-
-
-
-
-
-
-
-
-
-
 
 
 //DO NOT EDIT BELOW THIS POINT
